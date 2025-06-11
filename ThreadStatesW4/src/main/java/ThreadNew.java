1b4d124ffc780961b2c94aa9c5cf2728d80c4cbe
Syaza <nur_syazalina_b@soc.uum.edu.my>
2025-06-11 23:05:12 +0800

    Delete ThreadStatesW4 directory

--- ThreadStatesW4/src/main/java/ThreadNew.java
+++
@@ -1,7 +0,0 @@
-public class ThreadNew {
-
-    public static void main(String[] args) {
-        Thread t = new Thread();
-        System.out.println(t.getState());
-    }
-}
--- ThreadStatesW4/src/main/java/ThreadRunnable.java
+++
@@ -1,8 +0,0 @@
-public class ThreadRunnable {
-
-    public static void main(String[] args) {
-        Thread t = new Thread();
-        t.start();
-        System.out.println(t.getState());
-    }
-}
