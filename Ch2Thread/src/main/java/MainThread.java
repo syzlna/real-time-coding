7c4057c4412d460dce78d2264ffb24942c4002f7
Syaza <nur_syazalina_b@soc.uum.edu.my>
2025-06-11 23:05:02 +0800

    Delete Ch2Thread directory

--- Ch2Thread/src/main/java/MainThread.java
+++
@@ -1,7 +0,0 @@
-public class MainThread {
-
-    public static void main(String[] args) {
-        System.out.println(Thread.currentThread().getName());
-        System.out.println(Thread.currentThread().getId());
-    }
-}
